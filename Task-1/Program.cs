dd8a6ee42074b7b6abb31ab6b8622ad452f24c16
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-26 01:18:16 +0300

    Initial commit

---
+++ Task-1/Program.cs
@@ -0,0 +1,73 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+Напишите программу, которая принимает на вход произвольное (пятизначное) число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+----------------------------------------------------------------------------------------------------------------------*/
+
+int FirstNumPart(int x) //function to split number to two parts and return the first
+{
+    int i=0;                        //index
+    int res=0;                      //result
+    int[] xDgt=new int[100];        //array to keep digits
+        
+    while(x>0)                      //splitting x to digits
+    {
+        xDgt[i++]= x%10;
+        x/=10;
+    }
+    for(int j=i-1;j>=i/2;j--)       //packing first half of the number to result
+    {
+        res=res+xDgt[j]*Convert.ToInt32(Math.Pow(10,j-i/2));
+    }
+    return res;
+}
+
+int SecondNumPart(int x)
+{
+    int i=0;                        //index
+
+
+
+
+    int res=0;                      //result
+    int[] xDgt=new int[100];        //array to keep digits
+    
+    while(x>0)                      //splitting x to digits
+    {
+        xDgt[i++]= x%10;
+        x/=10;
+    }
+    if(i%2==0)                      //checking if we have even either odd digits number
+    {
+        for(int j=0;j<i/2;j++)
+            res=res+xDgt[j]*Convert.ToInt32(Math.Pow(10,i/2-j-1)); //packing resut for even digits num
+    }
+    else
+        for(int j=0;j<=i/2;j++)
+            res=res+xDgt[j]*Convert.ToInt32(Math.Pow(10,i/2-j)); //packing resut for odd digits num
+    return res;
+}
+
+bool CheckPalindrom(int x)          //to split number into two parts and compare them
+{
+    int a,b;
+    a=FirstNumPart(x);
+    b=SecondNumPart(x);
+    return a==b;
+}
+
+
+while(true)  //main
+{
+    Console.Write("Введите число для проверки на палиндром или \"q\" для выхода: ");
+    string input = Console.ReadLine();
+    if(input.ToLower()=="q")
+        return;
+    int x = Convert.ToInt32(input);
+    bool isPalindrom = CheckPalindrom(x);
+    if(isPalindrom)
+        Console.WriteLine($"{FirstNumPart(x)} == {SecondNumPart(x)} => {isPalindrom}");
+    else
+        Console.WriteLine($"{FirstNumPart(x)} != {SecondNumPart(x)} => {isPalindrom}");
+}
---
+++ Task-2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+----------------------------------------------------------------------------------------------------------------------*/
+
+double Dist(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double dx = x2-x1;
+    double dy = y2-y1;
+    double dz = z2-z1;
+    return Math.Sqrt(dx*dx+dy*dy+dz*dz);
+}
+
+Console.Write("Введите координату х первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату х второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Расстояние между точками ({x1},{y1},{z1}) и ({x2},{y2},{z2}) : {Dist(x1,y1,z1,x2,y2,z2):F2}");
